5e450fef2f713309940088704f1219d592c8893b
Hang <egoism@gmail.com>
2022-09-11 19:54:09 -0400

    Add symlink sys call

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -115,6 +115,12 @@ sys_fstat(void)
   return filestat(f, st);
 }
 
+uint64
+sys_symlink(void) {
+
+return 0;
+}
+
 // Create the path new as a link to the same inode as old.
 uint64
 sys_link(void)
